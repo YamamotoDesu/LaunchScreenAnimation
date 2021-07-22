986e177d8e2d9b89fdcd39622df4b0f6c538b4f8
yamamoto <kyoyamamoto1202@gmail.com>
2021-07-22 23:01:53 +0900

    Initial Commit

---
+++ Launch Screen Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Launch Screen Animation
+//
+//  Created by 山本響 on 2021/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
